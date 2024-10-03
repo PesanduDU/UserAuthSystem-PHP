4242dafb6fe71b2645c2d71947d88c06c734fbe7
PesanduDU <pesandudubeysekara@gmail.com>
2024-10-03 10:43:27 +0530

    5th commit

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST['signup-submit'])){
+if (isset($_POST['signup-submit'])) {
 
     require 'dbhelper.inc.php';
 
@@ -9,15 +9,51 @@ if(isset($_POST['signup-submit'])){
     $userPassword = $_POST['password'];
     $userPasswordRepeat = $_POST['reenter_password'];
 
-    if(empty($userName) || empty($userEmail) || empty($userPassword) || empty($userPasswordRepeat)){
-        header("Location: ../signup.php?error=emptyfields&username=".$userName."&email=".$userEmail);
+    if (empty($userName) || empty($userEmail) || empty($userPassword) || empty($userPasswordRepeat)) {
+        header("Location: ../signup.php?error=emptyfields&username=" . $userName . "&email=" . $userEmail);
         exit();
-    }
-    elseif (!filter_var($userEmail,FILTER_VALIDATE_EMAIL)) {
-        header("Location: ../signup.php?error=invalidemail&username=".$userName);
+    } elseif (!filter_var($userEmail, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $userName)) {
+        header("Location: ../signup.php?error=invalidUsernameEmail");
         exit();
-    }elseif (!preg_match("/^[a-zA-Z0-9]*$/", $userName)){
-        header("Location: ../signup.php?error=invalidemail&username=".$userName);
+    } elseif (!filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
+        header("Location: ../signup.php?error=invalidemail&username=" . $userName);
         exit();
+    } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $userName)) {
+        header("Location: ../signup.php?error=invalidusername&useremail=" . $userEmail);
+        exit();
+    } elseif ($userPassword !== $userPasswordRepeat) {
+        header("Location: ../signup.php?error=passwordmismatch&username=" . $userName . "&email=" . $userEmail);
+        exit();
+    } else {
+        $sql = "SELECT user_name FROM users WHERE user_name=?";
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "s", $userName);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+
+            $checkResult = mysqli_stmt_num_rows($stmt);
+
+            if ($checkResult > 0) {
+                header("Location: ../signup.php?error=usernamealreadyexist&&email=" . $userEmail);
+                exit();
+            } else {
+                $sql = "INSERT INTO users (user_name, email, password) VALUES (?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                } else {
+                    mysqli_stmt_bind_param($stmt, "sss", $userName, $userEmail, $userPassword);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_store_result($stmt);
+                }
+            }
+        }
     }
 }
